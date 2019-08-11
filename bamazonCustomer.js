c37ee77169ef68ac9c935a4dc5b4b577c0312df6
Nelson <neskone@outlook.com>
2019-08-11 15:04:44 -0700

    Mysql working and create a video testing that work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+
 const connection = mysql.createConnection({
   host: "localhost",
 
@@ -21,10 +22,11 @@ connection.connect(function(err) {
   
 });
 
+
 // validateInput makes sure that the user is supplying only positive integers for their inputs
 function validateInput(value) {
-	var integer = Number.isInteger(parseFloat(value));
-	var sign = Math.sign(value);
+	const integer = Number.isInteger(parseFloat(value));
+	const sign = Math.sign(value);
 
 	if (integer && (sign === 1)) {
 		return true;
@@ -54,36 +56,34 @@ function promptUserPurchase() {
 			filter: Number
 		}
 	]).then(function(input) {
-		// console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
+		console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
 
-		var item = input.item_id;
-		var quantity = input.quantity;
+		const item = input.item_id;
+		const quantity = input.quantity;
 
 		// Query db to confirm that the given item ID exists in the desired quantity
-		var queryStr = 'SELECT * FROM products WHERE ?';
+		const queryStr = 'SELECT * FROM products WHERE ?';
 
 		connection.query(queryStr, {item_id: item}, function(err, data) {
 			if (err) throw err;
 
-			// If the user has selected an invalid item ID, data attay will be empty
-			// console.log('data = ' + JSON.stringify(data));
-
+			
 			if (data.length === 0) {
 				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
 				displayInventory();
 
 			} else {
-				var productData = data[0];
+				const productData = data[0];
 
-				// console.log('productData = ' + JSON.stringify(productData));
-				// console.log('productData.stock_quantity = ' + productData.stock_quantity);
+				//console.log('productData = ' + JSON.stringify(productData));
+				 //console.log('productData.stock_quantity = ' + productData.stock_quantity);
 
 				// If the quantity requested by the user is in stock
 				if (quantity <= productData.stock_quantity) {
 					console.log('Congratulations, the product you requested is in stock! Placing order!');
 
 					// Construct the updating query string
-					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+					const updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
 					// console.log('updateQueryStr = ' + updateQueryStr);
 
 					// Update the inventory
@@ -121,9 +121,9 @@ function displayInventory() {
 		if (err) throw err;
 
 		console.log('Existing Inventory: ');
-		console.log('...................\n');
+		console.log('...................................................................................\n');
 
-		var strOut = '';
+		let strOut = '';
 		for (var i = 0; i < data.length; i++) {
 			strOut = '';
 			strOut += 'Item ID: ' + data[i].item_id + '  //  ';
@@ -134,7 +134,7 @@ function displayInventory() {
 			console.log(strOut);
 		}
 
-	  	console.log("---------------------------------------------------------------------\n");
+	  	console.log("--------------------------------------------------------------------------------------\n");
 
 	  	//Prompt the user for item/quantity they would like to purchase
 	  	promptUserPurchase();
